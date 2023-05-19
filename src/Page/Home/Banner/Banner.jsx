d36e6179bf4d763f9cafac5e4cd1045b9174d771
abrahman002 <rahmanx661@gmai.com>
2023-05-19 16:17:44 +0600

    added gallery

--- src/Page/Home/Banner/Banner.jsx
+++ src/Page/Home/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
 
 
     return (
-        <div className="carousel w-full  h-[600px] rounded-lg">
+        <div className="carousel w-full md:h-[300px] lg:h-[600px] rounded-lg">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src="https://johnlewis.scene7.com/is/image/JohnLewis/baby_toys_cw_231221" className="w-full" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
--- src/Page/Home/Gallery/Gallery.jsx
+++ src/Page/Home/Gallery/Gallery.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import './Gallery.css'
+import { PhotoAlbum } from 'react-photo-album';
+
 
 const Gallery = () => {
+    const photos = [
+        {
+            src: "https://i.ytimg.com/vi/_dSL7zoUs6o/maxresdefault.jpg",
+            width: 800,
+            height: 600
+        },
+        {
+            src: "https://www.babycenter.com.au/ims/2017/01/iStock-475269220_wide.jpg",
+            width: 1600,
+            height: 900
+        },
+        {
+            src: "https://www.gentlenursery.com/wp-content/uploads/2017/12/non-toxic-toys-for-babies-and-toddlers-1600x900.jpg",
+            width: 400,
+            height: 400
+        },
+        {
+            src: "https://cdn.shopify.com/s/files/1/1445/6088/files/Untitled_design_18.png?v=1676119977&width=3000",
+            width: 400,
+            height: 400
+        },
+        {
+            src: "https://media.istockphoto.com/id/912734770/photo/baby-laughing-and-playing-with-toys-on-a-carpet.jpg?s=612x612&w=0&k=20&c=Gu7RC4rXEVlPb-Qfdr2FJMXTwtiFZztwseHOZZ1tuKo=",
+            width: 400,
+            height: 400
+        },
+        {
+            src: "https://cdn.cdnparenting.com/articles/2018/09/377726401-H.jpg",
+            width: 400,
+            height: 400
+        }
+    ];
     return (
-        <div className='mt-10'>
-            <h1 className='text-3xl font-bold text-center'>Image Gallery</h1>
+        <div className='mt-10 mb-10 md:'>
+            <h1 className='text-3xl font-bold text-center mb-5'>Image Gallery</h1>
+            <div className='images'>
+                <PhotoAlbum layout="rows" photos={photos} />
+
+            </div>
         </div>
     );
 };
