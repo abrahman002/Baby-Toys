d7fcc6689f8cddae86c0bd393bce24169e05d509
abrahman002 <rahmanx661@gmai.com>
2023-05-19 14:23:25 +0600

    added footer

---
+++ src/Page/Home/Gallery/Gallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div className='mt-10'>
+            <h1 className='text-3xl font-bold text-center'>Image Gallery</h1>
+        </div>
+    );
+};
+
+export default Gallery;
